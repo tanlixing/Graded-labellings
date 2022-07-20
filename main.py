52f060383aeed2a3cbce61532a1dad1608a9196b
tanlixing <tanlixing@126.com>
2022-07-20 09:47:21 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from PreferredSemantics import *
+from GroundedSemantics import *
+from SparseMatrix import *
+from scipy import sparse
+
+
+def testPr(argNum: int, m: int, n: int, data: sparse.csr_matrix):
+    pr = PreferredSemantics(argNum, data, m, n)
+    pr.EnumPr()
+
+
+def testGr(argNum: int, m: int, n: int, data: sparse.csr_matrix):
+    pr = GroundedSemantics(argNum, data, m, n)
+    pr.ComputGr()
+
+
+if __name__ == "__main__":
+    argNum = int(input('Enter the number of arguments:'))
+    m = int(input('Enter m:'))
+    n = int(input('Enter n:'))
+    myMatrix = SparseMatrix(argNum, 'pr.npz')
+    data = myMatrix.getData(0.25)
+    print(data.A)
+    #testGr(argNum, m, n, data)
+    testPr(argNum, m, n, data)
